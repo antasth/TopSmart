e06a5176066bff6b547dd64d17c01a48b1fcc0cd
antasth <antasth@mail.ru>
2023-01-31 23:42:18 +0300

    add dashed lines between names and prices in CartDetails component

--- src/components/CartDetails/CartDetails.jsx
+++ src/components/CartDetails/CartDetails.jsx
@@ -8,34 +8,40 @@ const CartDetails = () => {
   const cart = useContext(CartContext)
   return (
     <div className={styles.carttotal}>
+    <div className={styles.cartcontent}>
       <h2>Детали заказа</h2>
-      <div className={styles.carttotalitems}>
-        <div className={styles.carttotalitem}>
-          <p>
-            {`${cart.cartItems.length} ${
-              cart.cartItems.length === 0
-                ? ' товаров '
-                : cart.cartItems.length === 1
-                ? ' товар '
-                : cart.cartItems.length < 5
-                ? ' товара '
-                : ' товаров '
-            }`}
-          </p>
-          {slicePrice(cart.fullPrice)} ₽
-        </div>
-        <div className={styles.carttotalitem}>
-          <p>Скидка 5%</p>-{slicePrice(Math.floor((cart.fullPrice / 100) * 5))}{' '}
-          ₽
-        </div>
-        <div className={styles.cartfullprice}>
-          <p>Итого</p>
-          {slicePrice(
-            cart.fullPrice - Math.floor((cart.fullPrice / 100) * 5)
-          )}{' '}
-          ₽
-        </div>
-      <OrderButton className={styles.orderbutton}>Перейти к оформлению</OrderButton>
+      <ul className={styles.carttotalitems}>
+        <li>
+          {`${cart.cartItems.length} ${
+            cart.cartItems.length === 0
+              ? ' товаров '
+              : cart.cartItems.length === 1
+              ? ' товар '
+              : cart.cartItems.length < 5
+              ? ' товара '
+              : ' товаров '
+          }`}
+          <span className={styles.value}>{slicePrice(cart.fullPrice)} ₽</span>
+        </li>
+        <li>
+          Скидка 5%
+          <span className={styles.value}>
+            -{slicePrice(Math.floor((cart.fullPrice / 100) * 5))} ₽
+          </span>
+        </li>
+        <li className={styles.cartfullprice}>
+          Итого
+          <span className={styles.value}>
+            {slicePrice(
+              cart.fullPrice - Math.floor((cart.fullPrice / 100) * 5)
+            )}{' '}
+            ₽
+          </span>
+        </li>
+      </ul>
+      <OrderButton className={styles.orderbutton}>
+        Перейти к оформлению
+      </OrderButton>
       </div>
     </div>
   )
