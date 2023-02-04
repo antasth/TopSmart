d969bdbd73ab71d76698fb55bf99b0e936580813
antasth <antasth@mail.ru>
2023-02-04 16:41:27 +0300

    change slider thumbs to bottom left

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,14 +1,14 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Mousewheel, Pagination, Navigation,FreeMode, Thumbs } from 'swiper'
+import { Mousewheel, Pagination, Navigation, FreeMode, Thumbs } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import styles from './ProductPage.module.scss'
 
 const ProductPage = () => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [imagesNavSlider, setImagesNavSlider] = useState(null)
   const device = useLocation()
   console.log(device.state)
   const { device_name, display_size, storage, ram, pictures } = device.state
@@ -18,65 +18,74 @@ const ProductPage = () => {
         {display_size} {device_name} {storage} {ram}
       </h1>
       <div className={styles.content}>
-        <div className={styles.slider}>
-        <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
-        mousewheel={true}
-        loop={true}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[Mousewheel, FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        {pictures.map((picture) => (
-              <SwiperSlide>
-                <img src={picture} alt="phoneimg" />
-              </SwiperSlide>
-            ))}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
-      >
-        {pictures.map((picture) => (
-              <SwiperSlide>
-                <img src={picture} alt="phoneimg" />
-              </SwiperSlide>
-            ))}
-      </Swiper>
-          {/* <Swiper
-            mousewheel={true}
-            direction={'horizontal'}
-            slidesPerView={1}
-            spaceBetween={0}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Mousewheel, Pagination, Navigation]}
-            className={styles.swiper}
-          >
-            {pictures.map((picture) => (
-              <SwiperSlide>
-                <img src={picture} alt="phoneimg" />
-              </SwiperSlide>
-            ))}
-          </Swiper> */}
-        </div>
-        <div className={styles.devicecard}>
-        {display_size} {device_name} {storage} {ram}
-        </div>
+        <section className={styles.slider}>
+          <div className={styles.slider__flex}>
+            <div className={styles.slider__col}>
+              <div className={styles.slider__thumbs}>
+                <Swiper
+                  onSwiper={setImagesNavSlider}
+                  direction="vertical"
+                  spaceBetween={15}
+                  slidesPerView={8}
+                  mousewheel={true}
+                  className={styles.swiper_container1}
+                  breakpoints={{
+                    0: {
+                      direction: 'horizontal',
+                    },
+                    768: {
+                      direction: 'vertical',
+                    },
+                  }}
+                  modules={[Navigation, Thumbs, Mousewheel]}
+                >
+                  {pictures.map((slide, index) => {
+                    return (
+                      <SwiperSlide key={index}>
+                        <div className={styles.slider__image}>
+                          <img src={slide} alt="" />
+                        </div>
+                      </SwiperSlide>
+                    )
+                  })}
+                </Swiper>
+              </div>
+            </div>
+
+            <div className={styles.slider__images}>
+              <Swiper
+                thumbs={{ swiper: imagesNavSlider }}
+                direction="horizontal"
+                slidesPerView={1}
+                spaceBetween={32}
+                mousewheel={true}
+                breakpoints={{
+                  0: {
+                    direction: 'horizontal',
+                  },
+                  768: {
+                    direction: 'horizontal',
+                  },
+                }}
+                className={styles.swiper_container2}
+                modules={[Navigation, Thumbs, Mousewheel]}
+              >
+                {pictures.map((slide, index) => {
+                  return (
+                    <SwiperSlide key={index}>
+                      <div className={styles.slider__image}>
+                        <img src={slide} alt="" />
+                      </div>
+                    </SwiperSlide>
+                  )
+                })}
+              </Swiper>
+            </div>
+          </div>
+        </section>
+        <section className={styles.devicecard}>
+          {display_size} {device_name} {storage} {ram}
+        </section>
       </div>
     </div>
   )
