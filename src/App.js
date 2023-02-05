af80e789dee7b7e67a00643bf60d56e63cd65a3a
antasth <antasth@mail.ru>
2023-02-05 15:09:23 +0300

    add ScrollToTop component to scroll on top then change pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { CartContextProvider } from './context/CartContext'
 import { FavContextProvider } from './context/FavContext'
 import { AppRouter } from './components/UI/AppRouter'
+import { ScrollToTop } from './helpers/ScrollToTop'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 
@@ -11,14 +12,16 @@ function App() {
     <ChakraProvider resetCSS={false}>
       <CartContextProvider>
         <FavContextProvider>
-          <BrowserRouter>
+          <Router>
+            <ScrollToTop/>
             <Header />
             <AppRouter />
             <Footer />
-          </BrowserRouter>
+          </Router>
         </FavContextProvider>
       </CartContextProvider>
     </ChakraProvider>
+
   )
 }
 
