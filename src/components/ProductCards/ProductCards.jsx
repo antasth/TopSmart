1b8cea99fc3afbf4b9588c68fab175348dcb4b8d
antasth <antasth@mail.ru>
2023-02-07 22:59:58 +0300

    add delivery options on ProductPage

--- src/components/ProductCards/ProductCards.jsx
+++ src/components/ProductCards/ProductCards.jsx
@@ -5,6 +5,10 @@ import { GiReceiveMoney } from 'react-icons/gi'
 import { TbReportMoney } from 'react-icons/tb'
 import { FiPercent } from 'react-icons/fi'
 import { IoChevronForward } from 'react-icons/io5'
+import { TbTruckDelivery } from 'react-icons/tb'
+import { GiPostOffice } from 'react-icons/gi'
+import { IoWalk } from 'react-icons/io5'
+import { RiPoliceCarFill } from 'react-icons/ri'
 import { slicePrice } from '../../Utils/PageFunctions'
 import styles from './ProductCards.module.scss'
 
@@ -30,7 +34,7 @@ const ProductCards = ({ device }) => {
 
       <div className={styles.bonuscard}>
         <GiReceiveMoney className={styles.bonusicon} />
-        <p>Кэшбэк +{Math.round(device.prices / 100)} бонусных рублей</p>
+        <p>Кэшбэк +{Math.round(device.prices / 100)} бонусов</p>
         <IoChevronForward className={styles.bonusarrow} />
       </div>
 
@@ -44,6 +48,30 @@ const ProductCards = ({ device }) => {
         <TbReportMoney className={styles.bonusicon} />
         <p>Нашли дешевле ?</p>
         <IoChevronForward className={styles.bonusarrow} />
+      </div>
+
+      <div className={styles.receive}>
+        <div className={styles.title}>
+          <p>Способы получения заказа</p>
+        </div>
+        <div className={styles.options}>
+          <div className={styles.option}>
+            <IoWalk className={styles.bonusicon} />
+            <p>Самовывоз из магазинов TopSmart Сегодня, бесплатно</p>
+          </div>
+          <div className={styles.option}>
+            <GiPostOffice className={styles.bonusicon} />
+            <p>Забрать на Почте России с 09.02, бесплатно</p>
+          </div>
+          <div className={styles.option}>
+            <TbTruckDelivery className={styles.bonusicon} />
+            <p>Курьерская доставка Завтра, бесплатно</p>
+          </div>
+          <div className={styles.option}>
+            <RiPoliceCarFill className={styles.bonusicon} />
+            <p>Экспресс-доставка от 2 часов сегодня</p>
+          </div>
+        </div>
       </div>
     </section>
   )
