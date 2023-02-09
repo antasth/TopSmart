50a3f54974b1b98031b103cb8942b1fdfcf22b37
antasth <antasth@mail.ru>
2023-02-09 23:33:25 +0300

    add services tabs to ProductSpecs

--- src/components/ProductSpecs/ProductSpecs.jsx
+++ src/components/ProductSpecs/ProductSpecs.jsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import { FavContext } from '../../context/FavContext'
 import { ProductRating } from '../UI/ProductRating/ProductRating'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
 import {
   IoStatsChartSharp,
   IoHeartOutline,
@@ -24,7 +26,7 @@ const ProductSpecs = ({ device }) => {
   return (
     <section className={styles.devicecard}>
       <ul className={styles.devicespecs}>
-        <h3>Основные характеристики</h3>
+        <h3 className={styles.specsheader}>Основные характеристики</h3>
         <li>
           Экран
           <span>{display_size}</span>
@@ -69,6 +71,44 @@ const ProductSpecs = ({ device }) => {
           />
           <IoShareSocialOutline className={styles.icon} />
         </div>
+      </div>
+      <div className={styles.services}>
+        <Tabs variant="enclosed">
+          <TabList>
+            <Tab>
+              <h3>Услуги</h3>
+            </Tab>
+            <Tab>
+              <h3>Гарантия</h3>
+            </Tab>
+            <Tab>
+              <h3>Аксессуары</h3>
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>Защита Экрана 12 мес.</p>
+              <p>Изготовление+наклейка пленки на 1 сторону смартфона </p>
+              <p>Наклейка стекла на смартфон </p>
+              <p>Комплект приложений "Базовый" </p>
+            </TabPanel>
+            <TabPanel>
+              <p>Гарантия от производителя12 мес. 0 ₽</p>
+              <p>Доп. гарантия+ 12 мес. 6 630 ₽ </p>
+              <p>
+                Страховка для техники 2 года. Отремонтируем за 24 часа или
+                обменяем на новую.
+              </p>
+            </TabPanel>
+            <TabPanel>
+              <p>
+                Чехол-накладка для смартфона в подарок (при покупке смартфона от
+                15 000 ₽)
+              </p>
+              <p>Защитное стекло для экрана</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </div>
     </section>
   )
